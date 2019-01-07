b41898c0850376cb4c3c9e1186283f3caf7fc447
Mounika_94 <mounika0906@yahoo.com>
2019-01-07 21:50:02 +0530

    Kafka logic implemented

--- src/main/java/com/prft/microservices/consumer/service/RetreiveDataServiceImpl.java
+++ src/main/java/com/prft/microservices/consumer/service/RetreiveDataServiceImpl.java
@@ -1,10 +1,9 @@
-/**
- * 
- */
 package com.prft.microservices.consumer.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
 
 import com.prft.microservices.consumer.model.Client;
 
@@ -12,13 +11,16 @@ import com.prft.microservices.consumer.model.Client;
  * @author mounika.pandiri
  *
  */
+@Service("retreiveDataServiceImpl")
 public class RetreiveDataServiceImpl implements RetreiveDataService {
 
 	protected Logger log = LoggerFactory.getLogger(RetreiveDataServiceImpl.class);
 
+	@KafkaListener(topics = "KafkaProducerTopic", groupId = "group_json", containerFactory = "clientKafkaListenerFactory")
 	public void retreiveDataFromTopic(Client client) {
 		log.info("Entering retreiveDataFromTopic()");
+		log.info("Consumed JSON Message: Client_Name: " + client.getName() + " Client_Status:  " + client.toString());
 		log.info("Exiting retreiveDataFromTopic()");
 	}
-	
+
 }
