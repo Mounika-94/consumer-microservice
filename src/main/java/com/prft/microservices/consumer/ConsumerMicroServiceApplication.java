119a7412a21c96a24e1289fef41fb558a3e5cd1b
Mounika_94 <mounika0906@yahoo.com>
2019-01-07 20:18:12 +0530

    Initial commit

---
+++ src/main/java/com/prft/microservices/consumer/ConsumerMicroServiceApplication.java
@@ -0,0 +1,14 @@
+package com.prft.microservices.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerMicroServiceApplication.class, args);
+	}
+
+}
+
